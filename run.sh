95c4254c5843fee15af728c8a555dacf97d2757d
kotekaman <abdul.haris.djafar@gmail.com>
2023-06-15 11:59:20 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS=/opt/credentils/google/keys.json
-export bq_project_id=quipper-school-analytics-dev
-export bq_dataset_id=tmp
-export bq_table_id=sg_taxi_availability
-while true;
-do
-    python main.py
-    sleep 2m
-done

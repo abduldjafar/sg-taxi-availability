9b060275f5c133ae7ecfae4b076c3755461220fc
kotekaman <abdul.haris.djafar@gmail.com>
2023-06-15 20:24:18 +0700

    Update sg_taxi_availability.py

--- data_sources/api/sg_taxi_availability.py
+++ data_sources/api/sg_taxi_availability.py
@@ -14,7 +14,7 @@ class ApiCall(object):
         data = {}
         try:
             data = response.json()
-            sleep(0.3)
+            sleep(0.2)
         except:
             print(response.text)
         return data
